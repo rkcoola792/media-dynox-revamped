e6b18af74303cbf27d6fc48afe0bd4f65d1ebe19
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-03-20 10:25:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App text-3xl'>
+      
+        APp
+    </div>
+  )
+}
+
+export default App
