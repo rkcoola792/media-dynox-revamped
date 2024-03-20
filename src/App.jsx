d1cf932024f04f513a1e293ab5af394b27b9d110
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-03-20 10:41:10 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Homepage from "./components/Homepage/Homepage";
+import Footer from "./components/Footer/Footer";
+import Test from "./components/Test/Test";
+
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Header></Header>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      children: [
+        { path: "/", element: <Homepage></Homepage> },
+        { path: "/marketing", element: <Test></Test> },
+      ],
+    },
+  ]);
 
   return (
-    <div className='App text-3xl'>
-      
-        APp
+    <div className="App ">
+      <RouterProvider router={router}></RouterProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='test'>
+      Test
+    </div>
+  )
+}
+
+export default Test
