f04559dd6861c48c38787768d24d995c32629e4e
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-24 21:40:38 +0530

    done

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -6,8 +6,8 @@ const Blogs = () => {
   return (
     <div className="blogs flex gap-8 relative  ">
       <div className="left relative">
-        <h1 className="lg:text-4xl sm:-ml-6 sm:text-2xl md:text-3xl text-3xl text-center sticky top-48 mb-[250px] mt-24">
-          <span className="font-semibold sm:-ml-5">Explore</span> recent Blogs
+        <h1 className="lg:text-4xl sm:-ml-6 sm:text-2xl md:text-3xl text-3xl text-left sticky top-48 mb-[250px] mt-24">
+          <span className="font-semibold ">Explore</span> recent Blogs
           for a view
         </h1>
       </div>
