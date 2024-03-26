c7eaf42d1a1b798eb2b349d464f05d20e885a1ea
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-03-26 15:42:54 +0530

    section 4 completed

--- src/components/Homepage/Section4.jsx
+++ src/components/Homepage/Section4.jsx
@@ -5,7 +5,7 @@ import FAQ from '../FAQ/FAQ'
 const Section4 = () => {
   return (
     <div className="left section-4 md:flex flex flex-col md:flex-row lg:px-24 px-12  bg-slate-100  py-24 mt-12 relative ">
-      <div className="sticky top-48 w-[50%]">
+      <div className="sticky top-48 sm:w-[50%]">
         <motion.div
           className="left lg:w-[65%] text-center md:text-left flex flex-col gap-8 sticky top-48 justify-center items-center"
           initial={{ opacity: 0, y: 75 }}
@@ -33,31 +33,31 @@ const Section4 = () => {
         </motion.div>
       </div>
 
-      <div className="right w-[50%]">
+      <div className="right sm:w-[50%]">
         <div className="right my-12 ">
           <div className="blogs sticky my-12 flex flex-col gap-4">
-            <div className="z-10 sticky top-48 right-12 bg-white rounded-lg">
+            <div className="z-10 sticky sm:top-48 top-[60%] right-12 bg-white rounded-lg">
               <FAQ
                 heading="Innovative Approach"
                 subheading="We mix equal parts of creative and strategic to bring you a phenomenal experience."
                 img="/FAQ1.webp"
               ></FAQ>
             </div>
-            <div className="z-20 bg-white sticky top-48 rounded-lg">
+            <div className="z-20 bg-white sticky sm:top-48 top-[60%] rounded-lg">
               <FAQ
                 heading="Futuristice Technology"
                 subheading="Our tech choices complement our ideas well, AI, Cloud, Sustainable Tech, we’re everywhere."
                 img="/FAQ2.jpeg"
               ></FAQ>
             </div>
-            <div className="z-30 bg-white sticky top-48 rounded-lg ">
+            <div className="z-30 bg-white sticky sm:top-48 top-[60%] rounded-lg ">
               <FAQ
                 heading="Benchmark Qaulity"
                 subheading="Quality is the virtue we swear all our work by. We strive to set the standards for the upcoming era."
                 img="/FAQ3.webp"
               ></FAQ>
             </div>
-            <div className="z-30 bg-white sticky top-48 rounded-lg ">
+            <div className="z-30 bg-white sticky sm:top-48 top-[60%] rounded-lg ">
               <FAQ
                 heading="100% Satisfaction"
                 subheading="We know we are simply great at what we do. But you don’t. Not until we satiate your hunger for ‘perfect’."
